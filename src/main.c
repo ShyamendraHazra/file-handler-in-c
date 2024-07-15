d5d87740561c776f22993a4810184963281260a2
Shyamendra Hazra <shyamendrahazra@gmail.com>
2024-07-15 12:19:43 +0530

    file-reader | v1.0.0

---
+++ src/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct {
+    char* ptr;
+    uint16_t buffer_length;
+} BUFFER;
+
+int main() {
+
+    char ch;
+    FILE* file_one;
+    BUFFER file_one_Buffer;
+    file_one_Buffer.ptr = NULL;
+    file_one_Buffer.buffer_length = 0;
+    file_one = fopen("assets/example.txt", "r");
+
+    if(file_one == NULL) {
+
+        printf("Could not open the file!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while((ch = fgetc(file_one)) != EOF) {
+        
+        char* temp;
+        
+        temp = realloc(file_one_Buffer.ptr, (sizeof(char)*file_one_Buffer.buffer_length+1));
+        
+        if(temp == NULL) {
+            printf("Failed to reallocate buffer space\n");
+            free(file_one_Buffer.ptr);
+            fclose(file_one);
+            exit(EXIT_FAILURE);
+        }
+
+        file_one_Buffer.ptr = temp;
+        file_one_Buffer.buffer_length++;
+
+        // printf("%d ",file_one_Buffer.buffer_length);
+        file_one_Buffer.ptr[file_one_Buffer.buffer_length-1] = ch;
+    }
+
+    char* temp = realloc(file_one_Buffer.ptr, (sizeof(char)*file_one_Buffer.buffer_length+1));
+        
+    if(temp == NULL) {
+        printf("Failed to reallocate buffer space\n");
+        free(file_one_Buffer.ptr);
+        fclose(file_one);
+        exit(EXIT_FAILURE);
+    }
+
+    file_one_Buffer.ptr = temp;
+    file_one_Buffer.buffer_length++;
+
+    file_one_Buffer.ptr[file_one_Buffer.buffer_length-1] = '\0';
+
+    printf("%s",file_one_Buffer.ptr);
+    fclose(file_one);
+    free(file_one_Buffer.ptr);
+    file_one_Buffer.buffer_length  = 0;
+    printf("\n");
+
+    return 0;
+}
+
